d08eb4f862b91c0dffa7c54d0f51b66df7736709
saraalite <saraalite@gmail.com>
2020-09-13 18:56:22 +0200

    Form in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,63 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import '../stylesheets/contact.scss';
 import github from '../assets/images/icons/github.svg';
 import email from '../assets/images/icons/email.svg';
 import linkedin from '../assets/images/icons/linkedin.svg';
 
+import { ReactComponent as ContactBackground } from '../assets/images/backgrounds/contact.svg';
+
 function Contact() {
+  const [chancheColorFormAndImage, setChancheColorFormAndImage] = useState(
+    false
+  );
   const { t } = useTranslation();
   return (
-    <main id="contact" className="contact-section">
-      <h1 className="contact-title">{t('menu.nav-contact')}</h1>
-      <section className="contact-flex">
-        <div className="contact-text">{t('contact.intro')}</div>
-        <div className="socialmedia-container">
-          <article className="contact-block">
-            <a href="https://github.com/saraalite" target="_blank">
-              <div className="contact-image-container">
-                <img className="contact-icon" src={github} alt="github logo" />
-              </div>
-            </a>
-            <a href="https://github.com/saraalite" target="_blank">
-              <p className="contact-url">https://github.com/saraalite</p>
-            </a>
-          </article>
-          <article className="contact-block">
-            <a
-              href="https://www.linkedin.com/in/saraalitevillodre/"
-              target="_blank"
-            >
-              <div className="contact-image-container">
-                <img
-                  className="contact-icon"
-                  src={linkedin}
-                  alt="linkedin logo"
-                />
-              </div>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/saraalitevillodre/"
-              target="_blank"
-            >
-              <p className="contact-url">
-                https://www.linkedin.com/in/ saraalitevillodre/
-              </p>
-            </a>
-          </article>
-          <article className="contact-block">
-            <a href="mailto:saraalite@gmail.com" target="_blank">
-              <div className="contact-image-container">
-                <img className="contact-icon" src={email} alt="email icon" />
-              </div>
-            </a>
-            <a href="mailto:saraalite@gmail.com" target="_blank">
-              <p className="contact-url">saraalite@gmail.com</p>
-            </a>
-          </article>
+    <div
+      onTouchStart={() => setChancheColorFormAndImage(true)}
+      onTouchEnd={() => setChancheColorFormAndImage(false)}
+      onMouseEnter={() => setChancheColorFormAndImage(true)}
+      onMouseLeave={() => setChancheColorFormAndImage(false)}
+      style={{
+        '--change-background': chancheColorFormAndImage
+          ? 'var(--primary-color)'
+          : 'var(--fourth-color)',
+      }}
+    >
+      <main id="contact" className="contact-section">
+        <ContactBackground className="contact-background" />
+        <h1 className="contact-title">{t('menu.nav-contact')}</h1>
+        <div className="contact-wrapper">
+          <section className="contact-flex">
+            <div className="contact-text">{t('contact.intro')}</div>
+            <div className="socialmedia-container">
+              <article className="contact-block">
+                <a href="https://github.com/saraalite" target="_blank">
+                  <div className="contact-image-container">
+                    <img
+                      className="contact-icon"
+                      src={github}
+                      alt="github logo"
+                    />
+                  </div>
+                </a>
+                <a href="https://github.com/saraalite" target="_blank">
+                  <p className="contact-url">https://github.com/saraalite</p>
+                </a>
+              </article>
+              <article className="contact-block">
+                <a
+                  href="https://www.linkedin.com/in/saraalitevillodre/"
+                  target="_blank"
+                >
+                  <div className="contact-image-container">
+                    <img
+                      className="contact-icon"
+                      src={linkedin}
+                      alt="linkedin logo"
+                    />
+                  </div>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/saraalitevillodre/"
+                  target="_blank"
+                >
+                  <p className="contact-url">
+                    https://www.linkedin.com/in/saraalitevillodre/
+                  </p>
+                </a>
+              </article>
+              <article className="contact-block">
+                <a href="mailto:saraalite@gmail.com" target="_blank">
+                  <div className="contact-image-container">
+                    <img
+                      className="contact-icon"
+                      src={email}
+                      alt="email icon"
+                    />
+                  </div>
+                </a>
+                <a href="mailto:saraalite@gmail.com" target="_blank">
+                  <p className="contact-url">saraalite@gmail.com</p>
+                </a>
+              </article>
+            </div>
+          </section>
         </div>
-      </section>
-    </main>
+      </main>
+    </div>
   );
 }
 
