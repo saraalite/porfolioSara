cc3f04ad649d082df291ec0562ad40589f9fd5c7
saraalite <saraalite@gmail.com>
2020-09-13 11:14:17 +0200

    Bot and about padding

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -6,49 +6,35 @@ import { ReactComponent as Person } from '../assets/images/bot.svg';
 
 function Bot() {
   const { t } = useTranslation();
-  const [isTalking, setIsTalking] = useState('');
+  const [isTalking, setIsTalking] = useState('bot.thing1');
   const [timesTalk, setTimesTalk] = useState(0);
-  const [timeOut, setTimesOut] = useState(setTimeout(() => {}, 0));
+  const [timeOut, setTimesOut] = useState(() => {});
 
-  const thingToSay = ['bot.thing1', 'bot.thing2', 'bot.thing3'];
+  const thingToSay = ['bot.thing2', 'bot.thing3'];
 
   const saySomething = () => {
     let nextToSay = timesTalk;
-    let nextTimeToTalk = timesTalk + 1;
     if (timesTalk > thingToSay.length - 1) {
       nextToSay = 0;
-      nextTimeToTalk = 0;
     }
     setIsTalking(thingToSay[nextToSay]);
-    setTimesTalk(nextTimeToTalk);
+    setTimesTalk(nextToSay + 1);
     clearTimeout(timeOut);
 
     setTimesOut(
       setTimeout(() => {
         setIsTalking('');
-      }, 1000)
+      }, 2000)
     );
   };
 
   return (
     <>
-      <div className="dialog">{t(isTalking)}</div>
+      {isTalking !== '' && <div className="dialog">{t(isTalking)}</div>}
       <div className="bot">
         {(isTalking === '' && (
-          <Person
-            onTouchMove={saySomething}
-            onTouchStart={saySomething}
-            onClick={saySomething}
-            className="bot person"
-          />
-        )) || (
-          <PersonHello
-            onTouchMove={saySomething}
-            onTouchStart={saySomething}
-            onClick={saySomething}
-            className="bot person"
-          />
-        )}
+          <Person onClick={saySomething} className="bot person" />
+        )) || <PersonHello onClick={saySomething} className="bot person" />}
       </div>
     </>
   );
